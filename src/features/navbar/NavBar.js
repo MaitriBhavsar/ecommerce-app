e79e518c06f192d4771e4c5cddc0de9b519c1e12
Maitri Bhavsar <maitribhavsar23@gmail.com>
2024-01-31 12:21:02 +0530

    cart page added

--- src/features/navbar/NavBar.js
+++ src/features/navbar/NavBar.js
@@ -1,6 +1,7 @@
 import React, { Children, Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, ShoppingCartIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Link } from 'react-router-dom'
 
 const user = {
     name: 'Tom Cook',
@@ -61,6 +62,7 @@ const NavBar = ({ children }) => {
                                     </div>
                                     <div className="hidden md:block">
                                         <div className="ml-4 flex items-center md:ml-6">
+                                            <Link to="/cart">
                                             <button
                                                 type="button"
                                                 className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -72,6 +74,7 @@ const NavBar = ({ children }) => {
                                             <span className="inline-flex items-center rounded-md mb-5 bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
                                                 3
                                             </span>
+                                            </Link>
                                             {/* Profile dropdown */}
                                             <Menu as="div" className="relative ml-3">
                                                 <div>
@@ -152,6 +155,7 @@ const NavBar = ({ children }) => {
                                             <div className="text-base font-medium leading-none text-white">{user.name}</div>
                                             <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
                                         </div>
+                                        <Link to="/cart">
                                         <button
                                             type="button"
                                             className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -163,6 +167,7 @@ const NavBar = ({ children }) => {
                                         <span className="inline-flex items-center rounded-md mb-5 bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
                                             3
                                         </span>
+                                        </Link>
                                     </div>
                                     <div className="mt-3 space-y-1 px-2">
                                         {userNavigation.map((item) => (
