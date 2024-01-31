b25b4a672a161db8e65fc108692cbe27761c7dd9
Maitri Bhavsar <maitribhavsar23@gmail.com>
2024-01-31 11:55:47 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -5,11 +5,35 @@ import './App.css';
 import { ProductList } from './features/product-list/ProductList';
 import NavBar from './features/navbar/NavBar';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <RouterProvider router={router} />
     </div>
   );
 }
